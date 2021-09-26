4adc3b0fed167437b66c01168f830aa660c56d96
Matheus Porto <matheusportoo@gmail.com>
2021-09-26 16:11:46 -0300

    add: swagger docs

--- src/Checkout/checkout.controller.ts
+++ src/Checkout/checkout.controller.ts
@@ -1,13 +1,20 @@
+import { ApiResponse, ApiTags } from '@nestjs/swagger';
 import { Controller, Post, Body } from '@nestjs/common';
 import { CheckoutApplication } from './checkout.application';
 import { CreateCartDtoRequest } from './dto/request/create-cart';
 import { CreateCartDtoResponse } from './dto/response/create-cart';
 
+@ApiTags('api')
 @Controller()
 export class CheckoutController {
   constructor(private readonly checkoutApplication: CheckoutApplication) {}
 
   @Post('/checkout')
+  @ApiResponse({
+    status: 200,
+    description: 'The found record',
+    type: CreateCartDtoResponse,
+  })
   createCart(
     @Body() createCartDtoRequest: CreateCartDtoRequest,
   ): Promise<CreateCartDtoResponse> {
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Checkout API')
+    .setDescription('Exclusive to create a checkout process')
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('swagger-ui.html', app, document);
+
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(3000);
 }
